9a362f3eae2254253bb8676783121af9919871a5
haruki7049 <tontonkirikiri@gmail.com>
2023-12-12 16:52:21 +0900

    fix: changing some expression

--- src/bin/ryst.rs
+++ src/bin/ryst.rs
@@ -1,5 +1,5 @@
 fn main() {
-    println!("Oh, perhaps you want to do programming using Rust-lang.");
-    println!("Then, You should go to 'https://www.rust-lang.org/'!!");
-    println!("There are many helpful for newbie rustacean as you.")
+    println!("Ah, perhaps you want to do programming using Rust-lang.");
+    println!("Then you should go to 'https://www.rust-lang.org/'!!");
+    println!("There is a lot of useful information there for beginner Rustacean like you.")
 }
